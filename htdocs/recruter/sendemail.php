15d18ba8f209576c90ac6283913a73328f1d03dd
Sebastien DIAZ <sebastien.diaz@gmail.com>
2019-05-19 16:28:31 +0200

    Update sendemail.php

--- htdocs/recruter/sendemail.php
+++ htdocs/recruter/sendemail.php
@@ -51,6 +51,9 @@ $twig = new \Twig\Environment($loader);
 
 echo $twig->render('index', ['name' => 'Fabien']);
 
+$sql = "EXPLAIN SELECT * from template where PK_TemplateEmail in (Select FK_Template from processustemplate WHERE FK_Processus in (SELECT FK_Processus from funnelprocessus where FK_Funnel in (Select PK_Funnel from funnel)))";
+
+
 mysqli_close($conn);
 
 
