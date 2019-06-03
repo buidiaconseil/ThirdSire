631a98155d6f2094d561ee6fb01b8b4ea466ccf6
Sebastien DIAZ <sebastien.diaz@gmail.com>
2019-06-03 07:19:01 +0200

    Update seosearch.php

--- seosearch.php
+++ seosearch.php
@@ -41,7 +41,9 @@ function crawl($url) {
 
   // exécution de curl
   $html_brut = curl_exec($ch);
-
+  if (curl_error($ch)) {
+    echo $url." ".$ch;
+}
   // fermeture de la session curl
   curl_close($ch);
 
@@ -96,9 +98,36 @@ function crawl($url) {
                     $value3 = str_replace("{", " ", $value3);
                     $value3 = str_replace("}", " ", $value3);
                     $value3 = str_replace(",", " ", $value3);
-                    $arr = array_merge($arr,preg_split("/ /", $value3));
+
+                    $listkey=preg_split("/ /", $value3);
+                    $i=0;
+                    $prevOne="";
+                    $prevSecond="";
+                    $finArray = array();
+                    echo "<br/>".count($listkey);
+                    foreach ($listkey as &$value) {
+                        if (strlen($value)>4){
+                            $finArray[]=$value;
+                        }
+                    }
+                    $listkey=$finArray;
+                    echo "<br/>".count($listkey);
+                    
+                    $finArray = array();
+                    
+                    foreach ($listkey as &$value) {
+                        if($i>0){
+                            $finArray[]=$prevOne." ".$value;
+                        }
+                        if($i>1){
+                            $finArray[]=$prevSecond." ".$prevOne." ".$value;
+                        }
+                        $i=$i+1;
+                    }
+                    $arr = array_merge($arr,$finArray);
                     
                 }
+                
             }
         }
     }
@@ -110,7 +139,7 @@ function crawl($url) {
 // déclaration de la fonction de crawl
 function cextends($key) {
 
-    $url="https://www.google.fr/complete/search?q=".$key."&cp=3&client=psy-ab&xssi=t&gs_ri=gws-wiz&hl=en-FR&authuser=0&psi=-Rr0XNTOEOqzgwfR_4CICA.1559501564815&ei=-Rr0XNTOEOqzgwfR_4CICA";
+    $url="https://www.google.fr/complete/search?q=".urlencode($key)."&cp=3&client=psy-ab&xssi=t&gs_ri=gws-wiz&hl=en-FR&authuser=0&psi=-Rr0XNTOEOqzgwfR_4CICA.1559501564815&ei=-Rr0XNTOEOqzgwfR_4CICA";
     // initialisation de curl
     $ch = curl_init($url);
     $arr=array();
@@ -118,34 +147,64 @@ function cextends($key) {
     //echo "<br/>P ".$temp." P<br/>";
     // création d'un fichier texte pour stocker le contenu crawlé
     // effacement du fichier précédent si existe
-    if(file_exists($temp)) {
-      unlink($temp);
-    }
+    //if(file_exists($temp)) {
+      //unlink($temp);
+    //}
   
-    $fp_fichier_html_brut = fopen($temp, 'a');
+    //$fp_fichier_html_brut = fopen($temp, 'a');
   
-    // définition des paramètres curl
-    // 1 redirection de l'output dans le fichier txt
-    curl_setopt($ch, CURLOPT_FILE, $fp_fichier_html_brut);
-  
-    // 2 on spécifie d'ignorer les headers HTTP
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-  
+    $header[0] = "Accept: text/xml,application/xml,application/xhtml+xml,"; 
+  $header[0] .= "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"; 
+  $header[] = "Cache-Control: max-age=0"; 
+  $header[] = "Connection: keep-alive"; 
+  $header[] = "Keep-Alive: 300"; 
+  $header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7"; 
+  $header[] = "Accept-Language: en-us,en;q=0.5"; 
+  $header[] = "Pragma: "; // browsers keep this blank. 
+  $temp = tempnam("/tmp", "FOO");
+  //echo "<br/>P ".$temp." P<br/>";
+  // création d'un fichier texte pour stocker le contenu crawlé
+  // effacement du fichier précédent si existe
+  if(file_exists($temp)) {
+    unlink($temp);
+  }
+
+  //$fp_fichier_html_brut = fopen($temp, 'a');
+
+  // définition des paramètres curl
+  // 1 redirection de l'output dans le fichier txt
+  //curl_setopt($ch, CURLOPT_FILE, $fp_fichier_html_brut);
+  curl_setopt($ch, CURLOPT_URL, $url); 
+  curl_setopt($ch, CURLOPT_USERAGENT, 'Googlebot/2.1 (+http://www.google.com/bot.html)'); 
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $header); 
+  curl_setopt($ch, CURLOPT_REFERER, 'http://www.google.com'); 
+  curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate'); 
+  curl_setopt($ch, CURLOPT_AUTOREFERER, true); 
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+  curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
     // exécution de curl
-    curl_exec($ch);
-  
+    $html_brut = curl_exec($ch);
+    
+    if (curl_error($ch)) {
+        echo $url." ".$ch;
+    }
+
     // fermeture de la session curl
     curl_close($ch);
   
     // fermeture du fichier texte
-    fclose($fp_fichier_html_brut);
+    //fclose($fp_fichier_html_brut);
   
   
     // passage du contenu du fichier à une variable pour analyse
-    $html_brut = file_get_contents($temp);
+    //$html_brut = file_get_contents($temp);
     $html_brut = str_replace("\u003cb", "", $html_brut);
     $html_brut = str_replace("\u003c\\/b", "", $html_brut);
     $html_brut = str_replace("\u003e", "", $html_brut);
+    $html_brut = str_replace("\u0026", "", $html_brut);
+    $html_brut = str_replace("#39;", "", $html_brut);
+    
     $html_brut = str_replace(")]}'", "", $html_brut);
     //echo "<br/>".$html_brut;
     $cutted=preg_split("/],/", $html_brut);
@@ -176,9 +235,21 @@ if( array_key_exists("url", $_GET)) {
 }
 $listkey=crawl($url);
 ///var_dump($listkey);
-$finArray = array();
+
+//$listkey=array_merge($finArray,$listkey);
+echo "<br/>".count($listkey);
 $i=0;
+$listkey=array_unique($listkey);
+echo "<br/>".count($listkey);
+
+$finArray = array();
+$prevOne="";
 foreach ($listkey as &$value) {
+    
+    
+    $prevSecond=$prevOne;
+    $prevOne=$value;
+    
    // echo $value;
     $finArray[$i]=$value;
     $i=$i+1;
