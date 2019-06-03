21ac079961f8f4417b64b20f03db55f07ee35830
Sebastien DIAZ <sebastien.diaz@gmail.com>
2019-06-03 23:13:52 +0200

    Update seosearch.php

--- seosearch.php
+++ seosearch.php
@@ -229,6 +229,64 @@ function cextends($key) {
       //var_dump($arr);
     //<meta name="keywords" content="mot clé 1, mot clé 2...">
   }
+
+  function crawlUrl($url) {
+    //$url="https://www.google.fr/complete/search?q=".urlencode($key)."&cp=3&client=psy-ab&xssi=t&gs_ri=gws-wiz&hl=en-FR&authuser=0&psi=-Rr0XNTOEOqzgwfR_4CICA.1559501564815&ei=-Rr0XNTOEOqzgwfR_4CICA";
+    // initialisation de curl
+    $ch = curl_init($url);
+    $arr=array();
+    //$temp = tempnam("/tmp", "FOO");
+    //echo "<br/>P ".$temp." P<br/>";
+    // création d'un fichier texte pour stocker le contenu crawlé
+    // effacement du fichier précédent si existe
+    //if(file_exists($temp)) {
+      //unlink($temp);
+    //}
+  
+    //$fp_fichier_html_brut = fopen($temp, 'a');
+  
+    $header[0] = "Accept: text/xml,application/xml,application/xhtml+xml,"; 
+  $header[0] .= "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"; 
+  $header[] = "Cache-Control: max-age=0"; 
+  $header[] = "Connection: keep-alive"; 
+  $header[] = "Keep-Alive: 300"; 
+  $header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7"; 
+  $header[] = "Accept-Language: en-us,en;q=0.5"; 
+  $header[] = "Pragma: "; // browsers keep this blank. 
+  $temp = tempnam("/tmp", "FOO");
+  //echo "<br/>P ".$temp." P<br/>";
+  // création d'un fichier texte pour stocker le contenu crawlé
+  // effacement du fichier précédent si existe
+  if(file_exists($temp)) {
+    unlink($temp);
+  }
+  //$fp_fichier_html_brut = fopen($temp, 'a');
+  // définition des paramètres curl
+  // 1 redirection de l'output dans le fichier txt
+  //curl_setopt($ch, CURLOPT_FILE, $fp_fichier_html_brut);
+  curl_setopt($ch, CURLOPT_URL, $url); 
+  curl_setopt($ch, CURLOPT_USERAGENT, 'Googlebot/2.1 (+http://www.google.com/bot.html)'); 
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $header); 
+  curl_setopt($ch, CURLOPT_REFERER, 'http://www.google.com'); 
+  curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate'); 
+  curl_setopt($ch, CURLOPT_AUTOREFERER, true); 
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+  curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    // exécution de curl
+    $html_brut = curl_exec($ch);
+    
+    if (curl_error($ch)) {
+        echo $url." ".curl_error($ch);
+    }
+    // fermeture de la session curl
+    curl_close($ch);
+  
+    
+   
+      return $html_brut;
+      //var_dump($arr);
+    //<meta name="keywords" content="mot clé 1, mot clé 2...">
+  }
 // on appelle une première fois la fonction avec l'url racine
 if( array_key_exists("url", $_GET)) {
     $url=$_GET["url"];
@@ -251,16 +309,41 @@ foreach ($listkey as &$value) {
    // echo $value;
     $finArray[$i]=$value;
     $i=$i+1;
-    $finNew=cextends($value);
-    foreach ($finNew as &$value2) {
-        $finArray[$i]=trim($value2);
-        $i=$i+1;
+    $trimed=trim($value);
+    
+    $url="https://suggestqueries.google.com/complete/search?client=chrome&q=".$trimed."&hl=fr&gl={isolg}";
+    $finNew=json_decode(crawlUrl($url));
+    $finArray2=array_merge($finArray, $finNew[1]);
+    $finArray=$finArray2;
+    $url="https://api.bing.com/osjson.aspx?language=FR-FR&form=OSDJAS&JsonType=json&query=".$trimed."&_=1559561814039";
+    $finNew=json_decode(crawlUrl($url));
+    $finArray2=array_merge($finArray, $finNew[1]);
+    $finArray=$finArray2;
+    $url="https://fr.search.yahoo.com/sugg/gossip/gossip-fr-ura/?output=fxjson&command=".$trimed."&appid=fp&nresults=20&_=1559561814041";
+    $finNew=json_decode(crawlUrl($url));
+    $finArray2=array_merge($finArray, $finNew[1]);
+    $finArray=$finArray2;
+
+    $url="https://fr.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&search=".$trimed."&namespace=0&limit=20&suggest=false&_=1559561814043";
+    $molo=crawlUrl($url);
+    
+    $finNew=json_decode($molo);
+    
+    if(!strpos($molo,"error")>-1){
+        $finArray2=array_merge($finArray, $finNew[1]);
+        $finArray=$finArray2;
     }
+    
+    //echo $finNew."<br/>";
+    //foreach ($finNew[] as &$value2) {
+    //    $finArray[$i]=trim($value2);
+    //    $i=$i+1;
+    //}
     //$finArray=array_merge($finArray, $finNew);
     
     
 }
-$finArray=array_unique($finArray);
+//$finArray=array_unique($finArray);
 //$out = array_values($finArray);
 //echo json_encode($out);
 //$newArray=array();
